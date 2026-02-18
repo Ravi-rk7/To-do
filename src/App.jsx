46e154cd4b3226d919d2e04f150402bb204cc8f1
Ravikiran Tandale <coding.ravikirantan7@gmail.com>
2026-02-18 18:11:33 +0530

    Added the container for form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
 const App = () => {
-  
+  const [todos,setTodos] = useState([]);
 
   return (
-    <div>
-      <h1>To-Do List</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-700">
+      <div className="bg-gray-800 p-6 rounded-xl w-175 shadow-lg ">
+        <h1 className="text-3xl font-bold mb-6 text-amber-100 text-center">My Todos</h1>
+      </div>
     </div>
   )
 }
